a0ffcceb5e5c9cdd4379bb63898be422a35e899e
tomekaq <t.antonik@sente.pl>
2015-07-23 10:17:00 +0200

    intial commit

---
+++ UsingTwoStream/MyStream.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace UsingTwoStream
+{
+    public class BCRandomStream : TextReader
+    {
+        Random rnd;
+        int rndrange;
+
+        public BCRandomStream(int range)
+        {
+            rndrange = range;
+            rnd = new Random();
+        }
+
+        public override int Read()
+        {
+            return rnd.Next(rndrange);
+        }
+    }
+}
+
+
---
+++ UsingTwoStream/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.Serialization.Formatters.Binary;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace UsingTwoStream
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var path = @"C:\Users\user\Documents\Zapis.txt";
+            var range = int.Parse(Console.ReadLine());
+            using (FileStream fileStream = new FileStream(path, FileMode.Open, FileAccess.Read))
+            {
+                using (StreamReader readStream = new StreamReader(fileStream))
+                {
+                    using (BCRandomStream rndstream = new BCRandomStream(range))
+                    {
+                        HashSet<int> listFile1 = new HashSet<int>();
+
+                        HashSet<int> hashSet = new HashSet<int>();
+
+                        char[] sep = new char[]{ '\r', '\n' };
+                        readStream.ReadToEnd()
+                            .Split(sep, StringSplitOptions.RemoveEmptyEntries)
+                            .Select(x => int.Parse(x))
+                            .ToList()
+                            .ForEach(x=>hashSet.Add(x));
+
+                      
+
+
+                        rndstream.Close();  
+                    }
+                    readStream.Close();
+                }
+                fileStream.Close();
+            }
+            Console.ReadLine();
+        }
+    }
+}
