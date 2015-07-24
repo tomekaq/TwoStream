c3049146ccd23ffe27494f98c6668168e255a577
tomekaq <t.antonik@sente.pl>
2015-07-24 12:27:38 +0200

    with intersect

--- UsingTwoStream/Program.cs
+++ UsingTwoStream/Program.cs
@@ -12,6 +12,13 @@ namespace UsingTwoStream
 {
     class Program
     {
+        static IEnumerable<int> RandomSource(BCRandomStream rs)
+        {
+            while (true)
+                yield return rs.Read();
+        }
+
+
         static void Main(string[] args)
         {
             var path = @"C:\Users\user\Documents\Zapis.txt";
@@ -23,28 +30,26 @@ namespace UsingTwoStream
                 {
                     using (BCRandomStream rndstream = new BCRandomStream(range))
                     {
-                        List<int> listFile = new List<int>();
-
-                        HashSet<int> hashSet = new HashSet<int>();
-
-                        HashSet<int> hashSet2 = new HashSet<int>();
                         char[] sep = new char[] { '\r', '\n' };
 
-                        List<int> ReadList = readStream.ReadToEnd()
+                        var ReadList = readStream.ReadToEnd()
                             .Split(sep, StringSplitOptions.RemoveEmptyEntries)
                             .Select(x => int.Parse(x))
-                            .ToList();
+                            .OrderBy(x => x).ToList();
+
+                        var GenerList = RandomSource(rndstream).Take(ReadList.Count())
+                            .OrderBy(x => x).ToArray();
+
+                        //linq porównaj i zwróć
+                        var lastList = ReadList.Intersect(GenerList).ToList();
                         
-                        ReadList.ForEach(x => hashSet.Add(x));
-
-                        Enumerable.Range(0, ReadList.Count)
-                            .Select(x => rndstream.Read())
-                            .Where(x => hashSet.Contains(x))
-                            .ToList()
-                            .ForEach(x =>hashSet2.Add(x));
-
-                        foreach (var t in hashSet2)
-                            Console.WriteLine(t);
+                        lastList
+                        .Select(x =>
+                        {
+                            Console.WriteLine("{0}", x);
+                            return x;
+                        })
+                        .ToList();
 
                         rndstream.Close();
                     }
